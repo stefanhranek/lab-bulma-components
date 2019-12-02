65eb03d36b3075f0518522ca363619bfaa6347d0
stefan hranek <hranekstefan@gmail.com>
2019-12-02 21:26:29 +0100

    done

---
+++ starter-code/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import SignUp from './SignUp';
+
+const App = () => (
+    <div className="App">
+        <SignUp/>
+    </div>
+)
+
+
+export default App;
---
+++ starter-code/src/Navbar.js
@@ -0,0 +1,73 @@
+import React from 'react';
+
+const buttonObj = {
+    isActive: 'is-active',
+    isBlack: 'is-black',
+    isCentered: 'is-centered',
+    isDanger: 'is-danger',
+    isDark: 'is-dark',
+    isFocused: 'is-focused',
+    isGrouped: 'is-grouped',
+    isHovered: 'is-hovered',
+    isInfo: 'is-info',
+    isInverted: 'is-inverted',
+    isLarge: 'is-large',
+    isLight: 'is-light',
+    isLink: 'is-link',
+    isLoading: 'is-loading',
+    isMedium: 'is-medium',
+    isOutlined: 'is-outlined',
+    isPrimary: 'is-primary',
+    isRight: 'is-right',
+    isRounded: 'is-rounded',
+    isSelected: 'is-selected',
+    isSmall: 'is-small',
+    isStatic: 'is-static',
+    isSuccess: 'is-success',
+    isText: 'is-text',
+    isWarning: 'is-warning',
+    isWhite: 'is-white',
+    };
+
+class Navbar extends React.Component {
+
+    render() {
+        return (
+            <div className="Navbar">
+                <nav className="navbar" role="navigation" aria-label="main navigation">
+                <div className="navbar-brand">
+                    <a className="navbar-item" href="https://bulma.io">
+                    <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"/>
+                    </a>
+                    <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    </a>
+                </div>
+                <div id="navbarBasicExample" className="navbar-menu">
+                    <div className="navbar-start">
+                    <a className="navbar-item">
+                        Home
+                    </a>
+                    </div>
+                    </div>
+                    <div className="navbar-end">
+                    <div className="navbar-item">
+                        <div className="buttons">
+                        <a className="button is-light">
+                            Log in
+                        </a>
+                        <a className="button is-primary">
+                            <strong>Sign up</strong>
+                        </a>
+                        </div>
+                    </div>
+                    </div>
+                </nav>
+            </div>
+        );
+    }
+}
+
+export default Navbar;
